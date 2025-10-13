338cbeca9603878175dae3c0d1e0f38645a39eb5
yuri <makarukyuri@gmail.com>
2025-10-13 12:46:16 +0300

    Fix broken links - update footer and homepage to link to Overview instead of intro

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -118,8 +118,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Overview',
+                to: '/docs/Overview',
               },
             ],
           },
